068e33607d0678608589a8d6df6c9ffbebf781d6
Thomas Berkane <thomas.berkane@epfl.ch>
2021-09-04 14:51:02 +0200

    improve table appearance

--- src/App.js
+++ src/App.js
@@ -1,15 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import MaterialTable from 'material-table';
+import MaterialTable, { MTableGroupRow } from 'material-table';
+import CustomGroupRow from './components/CustomGroupRow';
 
 export default function App() {
   const [data, setData] = useState([]);
 
+  /*  const columns = [
+    {
+      title: 'Group',
+      field: 'group',
+      defaultGroupOrder: 0,
+    },
+    {
+      title: 'Code',
+      field: 'code',
+    },
+    {
+      title: 'Name',
+      field: 'name',
+    },
+    {
+      title: 'Teachers',
+      field: 'teachers',
+    },
+    {
+      title: 'Section',
+      field: 'section',
+    },
+    {
+      title: 'Credits',
+      field: 'credits',
+      type: 'numeric',
+    },
+  ]; */
   const columns = [
-    { title: 'Code', field: 'code' },
-    { title: 'Name', field: 'name' },
-    { title: 'Teachers', field: 'teachers' },
-    { title: 'Section', field: 'section' },
-    { title: 'Credits', field: 'credits', type: 'numeric' },
+    {
+      title: 'Group',
+      field: 'group',
+      defaultGroupOrder: 0,
+      width: 0,
+      cellStyle: { whiteSpace: 'nowrap' },
+    },
+    {
+      title: 'Code',
+      field: 'code',
+      width: '10%',
+      cellStyle: { whiteSpace: 'nowrap' },
+    },
+    {
+      title: 'Name',
+      field: 'name',
+      width: '50%',
+      cellStyle: { whiteSpace: 'nowrap' },
+    },
+    {
+      title: 'Teachers',
+      field: 'teachers',
+      width: '30%',
+      cellStyle: { whiteSpace: 'nowrap' },
+    },
+    {
+      title: 'Section',
+      field: 'section',
+      width: '5%',
+      cellStyle: { whiteSpace: 'nowrap' },
+    },
+    {
+      title: 'Credits',
+      field: 'credits',
+      type: 'numeric',
+      width: '5%',
+      cellStyle: { whiteSpace: 'nowrap' },
+    },
   ];
 
   useEffect(() => {
@@ -19,13 +81,18 @@ export default function App() {
   }, []);
 
   return (
-    <div style={{ maxWidth: '100%' }}>
+    <div style={{ maxWidth: '60%', margin: 'auto' }}>
       <MaterialTable
         columns={columns}
         data={data}
-        title="Courses"
+        title="EPFLCourses"
         options={{
           filtering: true,
+          draggable: false,
+          paging: false,
+        }}
+        components={{
+          GroupRow: (props) => <CustomGroupRow {...props} />,
         }}
       />
     </div>
